ca1c00a20b48d5c2d29e975b115e0bc7983606e5
Katjahkrogh <you@example.com>
2023-08-30 18:15:13 +0200

    bar chart done

--- script.js
+++ script.js
@@ -1,19 +1,14 @@
 const list = document.querySelector("ul");
 
-const valueArray = [];
 setInterval(generateBarCharts, 1000);
 
 function generateBarCharts() {
   let rndNum = Math.floor(Math.random() * 101);
-  valueArray.push(rndNum);
-  if (valueArray.length >= 20) {
-    valueArray.shift();
-  }
   const li = document.createElement("li");
   li.style.setProperty("--height", rndNum);
   list.appendChild(li);
+  if (list.children.length > 20) {
+    list.removeChild(list.children[0]);
+  }
 }
 
-
-
-// Anden løsning skal li indkommenteres i html og css, så der fåes 20 søjler
